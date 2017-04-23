ab91e5d04c16305eabcd8970e7842686c9227d9e
Filip Szczepanski <dev@szczepanski.us>
2017-04-23 21:14:55 +0200

    Refactor: added class ChatServer

--- server.js
+++ server.js
@@ -1,124 +1,137 @@
 'use strict';
 
-var http = require('http');
-var io = require('socket.io');
+const http = require('http');
+const io = require('socket.io');
 
 const uuidV1 = require('uuid/v1');
 
-var users = [];
 
-
-function createServer() {
-    return new Promise((resolve, reject) => {
-        var server = http.createServer();
-        server.on('listening', () => resolve(server));
-        server.on('error', reject);
-        server.listen(3001);
-    });
-}
-
-createServer().then((server) => {
-    console.log('Server started!');
-    var socketio = io(server);
-    socketio.on('connection', socket => {
-
-      // client info
-      console.log(`new userID: ${socket.id}`);
-
-      //connection emit
-      socket.emit('hello', `Welcome!!!`);
-
-      //Events
-      socket.on('messageLine', ({messageLine, userName, hash}) => {
-
-        if (messageLine.length > 0 && messageLine[0] === '/') {
-          const commandLine = messageLine.slice(1)
-          const args = commandLine.split(' ');
-          const [ command, ...rest ] = args;
-
-          var name = rest[0];
-          var password = rest[1];
-
-          switch (command) {
-            case 'register':
-              const reg = registerUser(name, password);
-
-              let registerResult;
-              if (reg) {
-                registerResult = `${name} is registered!`;
-              } else {
-                registerResult = `${name} user exist!`;
-              }
-              socket.emit('registerResult', registerResult);
-
-              break;
-            case 'login':
-              socket.emit('logedin', {
-                name: name,
-                hash: loginUser(name, password),
-              });
-
-              if (loginUser(name, password)) {
-                socket.broadcast.emit('message', {
-                  name,
-                  messageLine:`${name} is logged in`,
-                 });
-              }
-              break;
-              case 'logout' :
-                logOut(hash);
-                socket.emit('logoutResult', 'You are logged out');
-              break;
-            default:
-          }
-        }
-
-        if (isUserExists(hash)) {
-          socket.broadcast.emit('message', {userName, messageLine});
-        }
-
+class ChatServer {
+  constructor(http, io) {
+    this.http = http;
+    this.io = io;
+    this.users = [];
+  }
+  createServer() {
+      return new Promise((resolve, reject) => {
+          var server = this.http.createServer();
+          server.on('listening', () => resolve(server));
+          server.on('error', (error)=> {
+            console.log(reject);
+          });
+          server.listen(3001);
       });
+  }
+  runServer() {
+    var createServer = this.createServer();
+    createServer.then((server) => {
+        console.log('Server started!');
+        var socketio = io(server);
+        socketio.on('connection', socket => {
 
+          // client info
+          console.log(`new userID: ${socket.id}`);
+
+          //connection emit
+          socket.emit('hello', `Welcome!!!`);
+
+          //Events
+          socket.on('messageLine', ({messageLine, userName, hash}) => {
+
+            if (messageLine.length > 0 && messageLine[0] === '/') {
+              const commandLine = messageLine.slice(1)
+              const args = commandLine.split(' ');
+              const [ command, ...rest ] = args;
+
+              var name = rest[0];
+              var password = rest[1];
+
+              switch (command) {
+                case 'register':
+                  const reg = this.registerUser(name, password);
+
+                  let registerResult;
+                  if (reg) {
+                    registerResult = `${name} is registered!`;
+                  } else {
+                    registerResult = `${name} user exist!`;
+                  }
+                  socket.emit('registerResult', registerResult);
+                break;
+
+                case 'login':
+                  hash = this.loginUser(name, password);
+                  socket.emit('logedin', {
+                    name: name,
+                    hash: hash,
+                  });
+
+                  if (hash) {
+                    socket.broadcast.emit('message', {
+                      userName: name,
+                      messageLine: `is logged in`,
+                     });
+                  }
+                  break;
+
+                  case 'logout' :
+                    this.logOut(hash);
+                    socket.emit('logoutResult', 'You are logged out');
+                  break;
+                default:
+              }
+            }
+
+            if (this.isUserExists(hash)) {
+              socket.broadcast.emit('message', {userName, messageLine});
+            }
+          });
+        });
+    }).catch((error) => {
+        console.log('error: ', error);
     });
-}).catch((error) => {
-    console.log('error: ', error);
-});
+  }
 
-function loginUser(name, password) {
-  for (const user of users) {
-     if (user.name === name && user.password === password) {
-       if (user.hash === null) {
-         const hash = uuidV1();
-         user.hash = hash;
+  loginUser(name, password) {
+    for (const user of this.users) {
+       if (user.name === name && user.password === password) {
+         if (user.hash === null) {
+           const hash = uuidV1();
+           user.hash = hash;
+         }
+         return user.hash;
        }
-       return user.hash;
-     }
-  }
-  return null;
-}
-
-function registerUser(name, password) {
-  for (const usr of users) {
-    if (usr.name === name) {
-      return false;
     }
+    return null;
   }
-  const user = new User(name, password);
-  users.push(user);
-  return true;
-}
 
-function isUserExists(hash) {
-  return users.some( u => {
-    return (hash && u.hash === hash);
-  });
-}
+  registerUser(name, password) {
+    for (const usr of this.users) {
+      if (usr.name === name) {
+        return false;
+      }
+    }
+    const user = new User(name, password);
+    this.users.push(user);
+    return true;
+  }
 
-function logOut(hash) {
-    return users = users.filter( u => {
+  isUserExists(hash) {
+    return this.users.some( u => {
+      return (hash && u.hash === hash);
+    });
+  }
+
+  logOut(hash) {
+    return this.users = this.users.filter( u => {
       return u.hash !== hash;
     });
+  }
+
 }
+
+const chatServer = new ChatServer(http, io);
+chatServer.runServer();
 
 class User {
   constructor(name, password) {
